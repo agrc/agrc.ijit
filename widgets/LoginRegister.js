34a6aaf833441bd43a7b0b8a1127829ffbabef59
Scott Davis <scott.sheri@gmail.com>
2013-10-10 07:50:19 -0600

    Tokens are now sent with at least gp requests. Not sure about other services.

--- widgets/LoginRegister.js
+++ widgets/LoginRegister.js
@@ -1,163 +1,162 @@
 define([
-    'dojo/_base/declare', 
-    'dojo/_base/lang',
-    'dojo/_base/window',
+        'dojo/_base/declare',
+        'dojo/_base/lang',
+        'dojo/_base/window',
 
-    'dojo/request',
-    'dojo/dom-style',
-    'dojo/dom-construct',
-    'dojo/aspect',
+        'dojo/request',
+        'dojo/dom-style',
+        'dojo/dom-construct',
+        'dojo/aspect',
 
-    'dojo/text!./templates/LoginRegister.html',
+        'dojo/text!./templates/LoginRegister.html',
 
-    'dijit/_TemplatedMixin', 
-    'dijit/_WidgetBase', 
-    'dijit/_WidgetsInTemplateMixin',
+        'dijit/_TemplatedMixin',
+        'dijit/_WidgetBase',
+        'dijit/_WidgetsInTemplateMixin',
 
-    'esri/request',
+        'esri/request',
 
-    'ijit/widgets/_LoginRegisterSignInPane',
-    'ijit/widgets/_LoginRegisterRequestPane',
-    'ijit/widgets/_LoginRegisterForgotPane',
-    'ijit/widgets/_LoginRegisterLogout',
+        'ijit/widgets/_LoginRegisterSignInPane',
+        'ijit/widgets/_LoginRegisterRequestPane',
+        'ijit/widgets/_LoginRegisterForgotPane',
+        'ijit/widgets/_LoginRegisterLogout',
 
-    // no params
-    'dijit/layout/StackContainer',
-    'dijit/layout/ContentPane',
-    'jquery/jquery'
-],
+        // no params
+        'dijit/layout/StackContainer',
+        'dijit/layout/ContentPane',
+        'jquery/jquery'
+    ],
 
-function (
-    declare, 
-    lang,
-    win,
+    function(
+        declare,
+        lang,
+        win,
 
-    xhr,
-    domStyle,
-    domConstruct,
-    aspect,
+        xhr,
+        domStyle,
+        domConstruct,
+        aspect,
 
-    template,
+        template,
 
-    _TemplatedMixin, 
-    _WidgetBase, 
-    _WidgetsInTemplateMixin,
+        _TemplatedMixin,
+        _WidgetBase,
+        _WidgetsInTemplateMixin,
 
-    esriRequest,
+        esriRequest,
 
-    _LoginRegisterSignInPane,
-    _LoginRegisterRequestPane,
-    _LoginRegisterForgotPane,
-    _LoginRegisterLogout
+        _LoginRegisterSignInPane,
+        _LoginRegisterRequestPane,
+        _LoginRegisterForgotPane,
+        _LoginRegisterLogout
     ) {
-    // summary:
-    //      Works with agrc/ArcGisServerPermissionsProxy to allow users to register or login.
-    // requires:
-    //      jquery and bootstrap.js
-    return declare('widgets/LoginRegister', 
-        [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
-        widgetsInTemplate: true,
-        templateString: template,
-        baseClass: 'login-register',
+        // summary:
+        //      Works with agrc/ArcGisServerPermissionsProxy to allow users to register or login.
+        // requires:
+        //      jquery and bootstrap.js
+        return declare('widgets/LoginRegister', [_WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin], {
+            widgetsInTemplate: true,
+            templateString: template,
+            baseClass: 'login-register',
 
-        // modal: Bootstrap Modal Dialog
-        modal: null,
+            // modal: Bootstrap Modal Dialog
+            modal: null,
 
-        // signInPane: _LoginRegisterSignInPane
-        signInPane: null,
+            // signInPane: _LoginRegisterSignInPane
+            signInPane: null,
 
-        // requestPane: _LoginRegisterRequestPane
-        requestPane: null,
+            // requestPane: _LoginRegisterRequestPane
+            requestPane: null,
 
-        // forgotPane: _LoginRegisterRequestPane
-        forgotPane: null,
+            // forgotPane: _LoginRegisterRequestPane
+            forgotPane: null,
 
-        // logout: _LoginRegisterLogout
-        logout: null,
+            // logout: _LoginRegisterLogout
+            logout: null,
 
-        urls: {
-            base: '/permissionproxy/api',
-            signIn: '/authenticate/user',
-            request: '/user/register',
-            reset: '/user/resetpassword'
-        },
+            urls: {
+                base: '/permissionproxy/api',
+                signIn: '/authenticate/user',
+                request: '/user/register',
+                reset: '/user/resetpassword'
+            },
 
-        // token: String
-        //      The ArcGIS Server token
-        token: null,
-
-
-        // parameters passed in via the constructor
-
-        // appName: String
-        //      The name of the app in the permissionproxy database
-        //      e.g. 'pel' for 'app_pel' in the raven database
-        appName: null,
-
-        // logoutDiv: Dom Element
-        //      The dom element in which you want the logout control placed
-        logoutDiv: null,
+            // token: String
+            //      The ArcGIS Server token
+            token: null,
 
 
-        postCreate: function () {
-            // summary:
-            //      dom is ready
-            console.log(this.declaredClass + "::postCreate", arguments);
+            // parameters passed in via the constructor
 
-            var that = this;
+            // appName: String
+            //      The name of the app in the permissionproxy database
+            //      e.g. 'pel' for 'app_pel' in the raven database
+            appName: null,
 
-            // create panes
-            this.signInPane = new _LoginRegisterSignInPane({
-                url: this.urls.base + this.urls.signIn,
-                parentWidget: this
-            }, this.signInPaneDiv);
-            aspect.after(this.signInPane, 'onSubmitReturn', function (response) {
-                that.logout = new _LoginRegisterLogout({
-                    name: response.result.user.name
-                }, that.logoutDiv);
-                esriRequest.setRequestPreCallback(function (ioArgs) {
-                    ioArgs.content.token = that.token;
-                    return ioArgs;
+            // logoutDiv: Dom Element
+            //      The dom element in which you want the logout control placed
+            logoutDiv: null,
+
+
+            postCreate: function() {
+                // summary:
+                //      dom is ready
+                console.log(this.declaredClass + "::postCreate", arguments);
+
+                var that = this;
+
+                // create panes
+                this.signInPane = new _LoginRegisterSignInPane({
+                    url: this.urls.base + this.urls.signIn,
+                    parentWidget: this
+                }, this.signInPaneDiv);
+                aspect.after(this.signInPane, 'onSubmitReturn', function(response) {
+                    that.logout = new _LoginRegisterLogout({
+                        name: response.result.user.name
+                    }, that.logoutDiv);
+                    esriRequest.setRequestPreCallback(function(ioArgs) {
+                        ioArgs.content.token = that.token;
+                        return ioArgs;
+                    });
+                }, true);
+                this.requestPane = new _LoginRegisterRequestPane({
+                    url: this.urls.base + this.urls.request,
+                    parentWidget: this
+                }, this.requestPaneDiv);
+                this.forgotPane = new _LoginRegisterForgotPane({
+                    url: this.urls.base + this.urls.reset,
+                    parentWidget: this
+                }, this.forgotPaneDiv);
+                this.stackContainer.startup();
+
+                domConstruct.place(this.domNode, win.body());
+
+                // this is to make sure that bootstrap is loaded after jQuery
+                require(['bootstrap'], function() {
+                    that.modal = $(that.modalDiv).modal({
+                        backdrop: 'static',
+                        keyboard: false
+                    });
+
+                    // focus email text box when form is shown
+                    that.signInPane.emailTxt.focus();
                 });
-            }, true);
-            this.requestPane = new _LoginRegisterRequestPane({
-                url: this.urls.base + this.urls.request,
-                parentWidget: this
-            }, this.requestPaneDiv);
-            this.forgotPane = new _LoginRegisterForgotPane({
-                url: this.urls.base + this.urls.reset,
-                parentWidget: this
-            }, this.forgotPaneDiv);
-            this.stackContainer.startup();
+            },
+            goToPane: function(pane) {
+                // summary:
+                //      fires when the user clicks the "Request Access" link
+                // pane: _LoginRegisterPane
+                console.log(this.declaredClass + "::goToPane", arguments);
 
-            domConstruct.place(this.domNode, win.body());
+                this.stackContainer.selectChild(pane);
+                pane.focusFirstInput();
+            },
+            hideDialog: function() {
+                // summary:
+                //      hides the modal dialog
+                console.log(this.declaredClass + "::hideDialog", arguments);
 
-            // this is to make sure that bootstrap is loaded after jQuery
-            require(['bootstrap'], function () {
-                that.modal = $(that.modalDiv).modal({
-                    backdrop: 'static',
-                    keyboard: false
-                });
-
-                // focus email text box when form is shown
-                that.signInPane.emailTxt.focus();
-            });
-        },
-        goToPane: function (pane) {
-            // summary:
-            //      fires when the user clicks the "Request Access" link
-            // pane: _LoginRegisterPane
-            console.log(this.declaredClass + "::goToPane", arguments);
-        
-            this.stackContainer.selectChild(pane);
-            pane.focusFirstInput();
-        },
-        hideDialog: function () {
-            // summary:
-            //      hides the modal dialog
-            console.log(this.declaredClass + "::hideDialog", arguments);
-        
-            $(this.modalDiv).modal('hide');
-        }
+                $(this.modalDiv).modal('hide');
+            }
+        });
     });
-});
