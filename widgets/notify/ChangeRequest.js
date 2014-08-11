696fd1e9230d87c2b42f96e61cf804ab37c8d669
steveoh <sgourley@utah.gov>
2014-08-11 16:24:33 -0600

    fix redline request to be relative

--- widgets/notify/ChangeRequest.js
+++ widgets/notify/ChangeRequest.js
@@ -6,20 +6,20 @@ define([
     'dojo/_base/Color',
     'dojo/_base/array',
     'dojo/_base/json',
-    
+
     'dojo/dom-style',
     'dojo/dom-attr',
     'dojo/dom-class',
-    
+
     'dojo/on',
     'dojo/query',
     'dojo/string',
     'dojo/aspect',
     'dojo/request',
-    
+
     'dijit/_WidgetBase',
     'dijit/_TemplatedMixin',
-    
+
     'esri/toolbars/draw',
     'esri/symbols/SimpleLineSymbol',
     'esri/graphic'
@@ -31,42 +31,29 @@ define([
     Color,
     array,
     dojoJson,
-    
+
     style,
     domAttr,
     domClass,
-    
+
     on,
     query,
     dojoString,
     aspect,
     request,
-    
+
     widgetBase,
     templatedMixin,
-    
+
     Draw,
     SimpleLineSymbol,
     Graphic
 ) {
-    // description:
-    //      **Summary**: Allows a user to markup a map, give a description, and submit the shape and description for review.
-    //      <p>
-    //      **Owner(s)**: Steve Gourley
-    //      </p>
-    //      <p>
-    //      **Test Page**: widgets/tests/ChangeRequest.html
-    //      </p>
-    //      <p>
-    //      **Description**:
-    //      This widget hits the email service. A separate viewer page will show the markup made.
-    //      </p>
-    //      **Exceptions**:
-    //      </p>
-    //      <ul><li>none</li></ul>
-    //
-    // example:
-    // |    new ChangeRequest({map: map}, 'div');
+    // summary:
+    //      Allows a user to markup a map, give a description,
+    //      and submit the shape and description for review.
+    //      This widget hits the email service.
+    //      A separate viewer page will show the markup made.
 
     return declare([widgetBase, templatedMixin], {
         templateString: template,
@@ -94,7 +81,7 @@ define([
             console.info('agrc.ijit.widgets.notify.ChangeRequest::postMixInProperties', arguments);
 
             // default to use the map's graphics layer if none was passed in
-            if (!this.graphicsLayer && !! this.map) {
+            if (!this.graphicsLayer && !!this.map) {
                 this.graphicsLayer = this.map.graphics;
 
                 //if maps already loaded just set this first.
@@ -106,7 +93,7 @@ define([
             }
 
             // create symbol if none was provided in options
-            if (!this.symbol && !! this.map) {
+            if (!this.symbol && !!this.map) {
                 this.symbol = new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
                     new Color([255, 0, 0]), 1);
             }
@@ -118,20 +105,20 @@ define([
         postCreate: function() {
             console.info('agrc.ijit.widgets.notify.ChangeRequest::postCreate', arguments);
 
-            this.form_redline.onsubmit = function() {
+            this.formRedline.onsubmit = function() {
                 return false;
             };
 
             var scoped = this;
             this.own(
-                on(this.btn_submit, 'click', lang.hitch(this, 'submitRedline')),
-                on(this.btn_redline, 'click', lang.hitch(this, 'onDrawStart')),
+                on(this.btnSubmit, 'click', lang.hitch(this, 'submitRedline')),
+                on(this.btnRedline, 'click', lang.hitch(this, 'onDrawStart')),
                 aspect.after(this.toolbar, 'onDrawEnd', lang.hitch(this, 'onDrawEnd'), true),
                 aspect.after(this.toolbar, 'activate', function() {
-                    domAttr.set(scoped.btn_redline, 'disabled', true);
+                    domAttr.set(scoped.btnRedline, 'disabled', true);
                 }),
                 aspect.after(this.toolbar, 'deactivate', function() {
-                    domAttr.set(scoped.btn_redline, 'disabled', false);
+                    domAttr.set(scoped.btnRedline, 'disabled', false);
                 })
             );
         },
@@ -154,7 +141,7 @@ define([
 
             this.toolbar.deactivate();
 
-            //this.txt_description.focus();
+            //this.txtDescription.focus();
         },
 
         submitRedline: function() {
@@ -170,14 +157,14 @@ define([
                 this.graphicsLayer.remove(this._graphic);
             }
 
-            var description = this.txt_description.value;
+            var description = this.txtDescription.value;
 
             if (this.request) {
                 this.request.cancel('duplicate in flight');
                 this.request = null;
             }
 
-            this.btn_submit.innerHTML = 'Submitting...';
+            this.btnSubmit.innerHTML = 'Submitting...';
 
             this.request = this._invokeWebService({
                 description: description
@@ -206,7 +193,7 @@ define([
             query('.control-group', this.domNode).removeClass('error');
 
             return array.every([
-                    this.txt_description
+                    this.txtDescription
                 ],
                 function(tb) {
                     return that._isValid(tb);
@@ -240,7 +227,7 @@ define([
             //     Deferred
             console.info('agrc.ijit.widgets.notify.ChangeRequest::_invokeWebService', arguments);
 
-            var url = 'http://localhost/sendemailservice/notify';
+            var url = '/sendemailservice/notify';
 
             var options = {
                 email: {
@@ -258,7 +245,8 @@ define([
             };
 
             if (this._graphic && this._graphic.geometry) {
-                options.template.templateValues.link = 'http://localhost/experimental/GraphicViewer?center={{center}}&level={{level}}&redline={{redline}}';
+                options.template.templateValues.link = 'http://localhost/experimental/GraphicViewer?' +
+                    'center={{center}}&level={{level}}&redline={{redline}}';
                 options.template.templateValues.center = this.map.extent.getCenter().toJson();
                 options.template.templateValues.level = this.map.getLevel();
                 options.template.templateValues.redline = this._graphic.geometry.toJson();
@@ -277,8 +265,8 @@ define([
             console.info('agrc.ijit.widgets.notify.ChangeRequest::completed', arguments);
 
             this._graphic = null;
-            this.txt_description.value = '';
-            this.btn_submit.innerHTML = 'Thanks!';
+            this.txtDescription.value = '';
+            this.btnSubmit.innerHTML = 'Thanks!';
         },
 
         error: function() {
@@ -294,7 +282,7 @@ define([
 
             style.set(this.errorMsg, 'display', 'inline');
             domClass.add(this.errorMsg.parentElement.parentElement, 'error');
-            this.btn_submit.innerHTML = 'Whoops.';
+            this.btnSubmit.innerHTML = 'Whoops.';
         }
     });
 });
