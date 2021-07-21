bb40f8e894f8f45d4c08222b84ffd0c1074f5891
steveoh <sgourley@utah.gov>
2021-07-21 17:03:37 -0600

    chore(notify): remove center and level. simplify link

--- widgets/notify/ChangeRequest.js
+++ widgets/notify/ChangeRequest.js
@@ -267,10 +267,7 @@ define([
             };
 
             if (this._graphic && this._graphic.geometry) {
-                options.template.templateValues.link = this.redliner +
-                    '?center={{center}}&level={{level}}&redline={{redline}}';
-                options.template.templateValues.center = JSON.stringify(this.map.extent.getCenter().toJson());
-                options.template.templateValues.level = this.map.getLevel();
+                options.template.templateValues.link = this.redliner;
                 options.template.templateValues.redline = JSON.stringify(this._graphic.geometry.toJson());
             }
 
